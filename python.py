b079b773e23448acd5754f0c697422439f75a095
まろたつ <urimaro1108@gmail.com>
2025-03-05 08:13:31 +0900

    ああ

--- python.py
+++ python.py
@@ -1,6 +1,13 @@
-import discord
-from discord.ext import commands
-from discord.ext.commands import Bot
+import glob
 import os
-import sys
+
+directory = "C:\\Users\\eitom\\OneDrive\\Desktop\\ganba" #os.path.expanduser("~/Desktop")
+
+
+files = glob.glob(f"{directory}\\*.webm")
+len = len(files)
+files_name = []
+for f in range(len):
+    files_name.append(os.path.basename(files[f]))
+    print(f"第{f+1}曲：『　{files_name[f]}　』\n")
 
